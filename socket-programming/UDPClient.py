7d2c7dea616bc080465a705943533d4924edb924
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-02-12 00:34:02 -0600

    UDP Client is done.

--- socket-programming/UDPClient.py
+++ socket-programming/UDPClient.py
@@ -1,5 +1,18 @@
-from socket impot *
+from socket import *
+
+serverName = "turing.uark.edu"
 
 serverPort = 12000
 
-#new line m
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+message = raw_input("Input lowercase sentence: ")
+
+clientSocket.sendto(message, (serverName, serverPort))
+
+modifiedMessage, serverAdress = clientSocket.recvfrom(2048)
+
+print modifiedMessage
+
+clientSocket.close()
+
